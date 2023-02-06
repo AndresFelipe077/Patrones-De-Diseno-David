d55640504c7bc4d60bb4692043c830ab9243fc5c
unknown <Aprendiz@POPDFTCCSFSM010.FormacionSena.RED>
2023-02-06 16:57:00 -0500

    cambio hecho en archivo de alex por juan

--- src/alexandergomez_/Apagar.java
+++ src/alexandergomez_/Apagar.java
@@ -3,6 +3,11 @@ package alexandergomez_;
 public class Apagar {
 	public void apagar() {
 		System.out.println("apagando....");
+		
+		for(int i =0;i>=10;i++) {
+			System.out.println("apagando por juan orozco");
+		}
+		
 	}
 
 }
