87f1dc97e986830adfa7ede5f8facd41f8a23d10
unknown <Aprendiz@POPDFTCCSFSM010.FormacionSena.RED>
2023-02-13 14:03:10 -0500

    cambio en el codigo alex por juan orozco

--- src/alexandergomez_/Apagar.java
+++ src/alexandergomez_/Apagar.java
@@ -2,7 +2,7 @@ package alexandergomez_;
 
 public class Apagar {
 	public void apagar() {
-		System.out.println("apagando....");
+		System.out.println("socio le3 falta codigo en la principal");
 		
 		for(int i =0;i>=10;i++) {
 			System.out.println("apagando por juan orozco");
